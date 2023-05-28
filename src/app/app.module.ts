6c28aa47b4065a33f7039900c6b64862aebcc903
Ray <raymondly96@gmail.com>
2023-05-28 01:03:19 -0700

    setup angular material color theme, converted css to scss, begin navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Material Components
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [   
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
+    // Material Components
+    MatToolbarModule,
+    // End Material Components
     BrowserModule,
     AppRoutingModule
   ],
